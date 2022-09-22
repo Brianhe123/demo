7be967e8156d704ced7e984017576350689a9099
Brianhe123 <supersciencefreak@gmail.com>
2022-09-21 22:47:50 -0400

    added new lines

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ We will test some git commands here and add new files
 2.
 3. We use this for class session 4 and 5 but also for homework 1
 4.
+5. This project keeps track of a scrip b575hw12b_script.sh  used to test bash commands
--- b575hw12b_script.sh
+++ b575hw12b_script.sh
@@ -10,3 +10,4 @@ sed 's/AGATCGGAAGAGC.*//' sample_rseq.txt > sample_rseq_CUT.txt
 echo "#== CUT ADAPTER DONE"
 diff sample_rseq_CUT.txt sample_cutadapt_rseq.txt
 echo "#== SCRIPT DONE"
+The last time the script was ran is Wed Sep 21 22:45:37 EDT 2022
